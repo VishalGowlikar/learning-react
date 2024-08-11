c2d1440f5fc90a70fb8a212748eddab59daab0b1
vishalGowlikar <vishalkishnn18886@gmail.com>
2024-08-11 10:07:25 -0400

    updated today morning on 11

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -8,20 +8,35 @@ export default function Textarea(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
-
+    const handleloClick = () =>{
+        console.log( "i have clicked the upper click button" + text)
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
     const handleOnchange = (event)=>{
         console.log("onChange")
         setText(event.target.value)
     }
-    const [text , setText]= useState('enter the text');
+    const [text , setText]= useState('');
     return (
-    <div>
+    <>
+    <div className='container'>
     <h1>{props.heading}</h1>
     <div className="my-3">
     <textarea className="form-control" value={text} onChange={handleOnchange}   id="myBox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary" onClick={handleUpClick}  >Convert to Uppercase</button>
+    <button className="btn btn-primary mx-2" onClick={handleUpClick}  >Convert to Uppercase</button>
+    <button className="btn btn-primary mx-2" onClick={handleloClick}  >Convert to Uppercase</button>
+
     </div>
+    <div className="container">
+        <h1>your text summary</h1>
+        <p>{text.split(' ').length} words and {text.length} characters</p>
+        <p>{0.008*text.split(' ').length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
             
     
   )
